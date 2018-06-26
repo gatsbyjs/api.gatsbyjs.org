7f92f0d6deacff23c390a62b8fb2c10a113b26a2
Jason Lengstorf <jason@lengstorf.com>
2018-06-26 00:17:12 -0700

    test: add stubs for remaining tests

--- server.mjs
+++ server.mjs
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import app from './app.mjs';
 import getLogger from './lib/logger.mjs';
 
 const logger = getLogger('server');
@@ -9,11 +8,8 @@ dotenv.config();
 const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 
-app.listen(port, () => {
-  logger.info(`started the server at http://%s:%d`, host, port);
-});
-
-process.on('unhandledRejection', error => {
-  logger.error('unhandledRejection');
-  logger.error(error);
-});
+export const startServer = app => {
+  app.listen(port, () => {
+    logger.info(`started the server at http://%s:%d`, host, port);
+  });
+};
