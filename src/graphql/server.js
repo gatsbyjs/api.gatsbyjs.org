d1831a5fd64c3963c96e9762f59d66c742be473b
Jason Lengstorf <jason@lengstorf.com>
2018-10-23 14:48:25 -0700

    feat: add auth check

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,12 +1,40 @@
 import express from 'express';
+import jwt from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
 import serverless from 'serverless-http';
 import graphiql from 'graphql-playground-middleware-express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import typeDefs from './schema.graphql';
 import resolvers from './resolvers';
 
+// We use Auth0 for validation, so we need to check that JWTs are valid.
+const requireValidJWT = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
 const app = express();
 
+// Make sure a valid token is present before doing anything else.
+app.use(requireValidJWT);
+
+// If there’s an auth error, send it as JSON so it’s useful in the GraphQL output.
+app.use((err, _, res, next) => {
+  if (err) {
+    res.json(err);
+  }
+
+  next();
+});
+
+// Set up the GraphQL server.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
