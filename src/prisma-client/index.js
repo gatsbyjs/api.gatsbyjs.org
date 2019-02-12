caab3fa30248ccfddcda1e131ba8c3c4d64772b1
Jason Lengstorf <jason@lengstorf.com>
2019-02-12 09:36:12 -0800

    fix: add a secret for the Prisma service

--- src/prisma-client/index.js
+++ src/prisma-client/index.js
@@ -1,17 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_lib_1 = require("prisma-client-lib");
-var typeDefs = require("./prisma-schema").typeDefs;
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prisma_lib_1 = require('prisma-client-lib');
+var typeDefs = require('./prisma-schema').typeDefs;
 
 var models = [
   {
-    name: "Contributor",
+    name: 'Contributor',
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://us1.prisma.sh/jason-lengstorf-418dad/api-gatsby-org/dev`
+  endpoint: `https://prisma.gatsbyjs.com`,
+  secret: `${process.env['PRISMA_SECRET']}`
 });
 exports.prisma = new exports.Prisma();
